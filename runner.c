b2708b63c6bf407da81be9caf1e360d43d6504e6
Mordecai-Wambua <mordecaingumbau@gmail.com>
2023-12-13 19:55:49 +0300

    task 0

--- runner.c
+++ runner.c
@@ -1,29 +1,81 @@
 #include "shell.h"
 
 /**
+ * token - tokenizer
+ * @cmd: pointer to command string
+ * @args: storage array
+ */
+void token(const char *cmd, char *args[])
+{
+	int a = 0;
+	char *token = strtok((char *)cmd, " ");
+
+	while (token != NULL && a < MAXARG - 1)
+	{
+		args[a++] = token;
+		token = strtok(NULL, " ");
+	}
+
+	args[a] = NULL;
+}
+
+/**
+ * exec - actual runner
+ * @args:array of tokens
+ */
+void exec(char *args[])
+{
+	pid_t c = fork();
+
+	if (c == -1)
+	{
+		perror("Fork Error!");
+		exit(FAIL);
+	}
+	else if (c == 0)
+	{
+		char *path = NULL;
+		char *env_path = getenv("PATH");
+		char *dir = strtok(env_path, ":");
+
+		while (dir != NULL)
+		{
+			char *tmp = malloc(strlen(dir) + strlen(args[0]) + 2);
+
+			strcpy(tmp, dir);
+			strcat(tmp, "/");
+			strcat(tmp, args[0]);
+			if (access(tmp, X_OK) == 0)
+			{
+				path = tmp;
+				break;
+			}
+			free(tmp);
+			dir = strtok(NULL, ":");
+		}
+
+		path = path ? path : args[0];
+		execve(path, args, environ);
+		perror("execve");
+		exit(FAIL);
+	}
+	else
+	{
+		int s;
+
+		waitpid(c, &s, 0);
+	}
+}
+
+/**
  * run - run acual commands
  * @cmd: user-input command
  */
 
 void run(const char *cmd)
 {
-	pid_t c = fork();
+	char *args[MAXARG];
 
-	if (c == -1)
-	{
-		perror("Fork error!");
-		exit(FAIL);
-	}
-	else if (c == 0)
-	{
-		execlp(cmd, cmd, (char *)NULL);
-		perror("execlp");
-		exit(FAIL);
-	}
-	else
-	{
-		int status;
-
-		waitpid(c, &status, 0);
-	}
+	token(cmd, args);
+	exec(args);
 }
--- shell.h
+++ shell.h
@@ -8,9 +8,14 @@
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #define SUCCESS 0
 #define FAIL 1
+#define MAXARG 100
 
 
 void writer(const char *message);
@@ -18,6 +23,8 @@ void prompt(void);
 void run(const char *cmd);
 void reader(char *cmd, size_t s);
 void printer(const char *message);
+void token(const char *cmd, char *args[]);
+void exec(char *args[]);
 
 
 #endif
